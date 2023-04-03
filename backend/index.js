ed8cafc5bd373471dcaa4a6b7ba64a7acdc9c77b
Ashish Kumar Rastogi <ashish.rastogi183@gmail.com>
2023-04-04 02:03:19 +0530

    deploy test2

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require("./db")
-mongoDB();
 
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
@@ -20,6 +19,9 @@ app.use('/api', require("./Routes/OrderData"));
 app.get('/',(req,res) => {
     res.send('Hello World! -----')
 })
-app.listen(port, () => {
-    console.log(`Exapmple app listen on ${port}`)
+
+mongoDB().then(()=>{
+    app.listen(port, () => {
+        console.log(`Exapmple app listen on ${port}`)
+    })
 })
