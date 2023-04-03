d459b610e4ca1ef516ced46ebb0c40c20d2b2259
Ashish Kumar Rastogi <ashish.rastogi183@gmail.com>
2023-04-04 03:13:34 +0530

    deploy test6

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -20,7 +20,7 @@ function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch(`https://calm-cyan-mite-tam.cyclic.app/api/orderData`, {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: "POST",
