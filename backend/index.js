9289d8670c64528cc2645874acddfad8472bedbc
Ashish Kumar Rastogi <ashish.rastogi183@gmail.com>
2023-05-02 23:15:54 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const mongoDB = require("./db")
 const app = express()
 const port = process.env.PORT || 5000
-const mongoDB = require("./db")
-
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","10.121.2.252");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', require("./Routes/CreatUser"));
